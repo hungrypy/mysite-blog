35b3984c9c1209329f05e6a7cbf6b6068ae6b6d8
yanwarsolah <yanwarsolah@gmail.com>
2016-07-08 07:51:15 +0700

    Fix remove comment (testing)

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
 	objects = models.Manager() # The default manager.
-	published = PublishedManager() # Our custom manager. manager customizing
+	published = PublishedManager() # Our custom manager
 	
 	class Meta:
 		ordering = ('-publish',)
